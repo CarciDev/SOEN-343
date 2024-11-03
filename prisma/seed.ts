efabb843c8527843a41d865cc709393561cc34e0
Brian Tkatch <brian@briantkatch.com>
2024-11-03 10:44:09 -0500

    Re-add seed content

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,29 +1,136 @@
-import {} from "@prisma/client";
-import { parseArgs } from "node:util";
+import { TrackingStatus, TransactionType, UserRole } from "@prisma/client";
+import { prisma } from "../src/lib/db/client.ts";
+import bcrypt from "bcrypt";
 
 ///// TEST / PREVIEW SEED /////
 
-async function seedAll() {}
+async function seedAll() {
+  const admin = await prisma.user.upsert({
+    where: { id: 1 },
+    update: {},
+    create: {
+      id: 1,
+      email: "admin@svelteship.test",
+      name: "Joe Admin",
+      comment: "Created from the Prisma seed file",
+      passwordHash: bcrypt.hashSync("admin123", 12),
+      role: UserRole.ADMIN,
+    },
+  });
+
+  const customer = await prisma.user.upsert({
+    where: { id: 2 },
+    update: {},
+    create: {
+      id: 2,
+      email: "customer@svelteship.test",
+      name: "First Customer",
+      comment: "Created from the Prisma seed file",
+      passwordHash: bcrypt.hashSync("first123", 12),
+      role: UserRole.CUSTOMER,
+    },
+  });
+
+  const box = await prisma.box.upsert({
+    where: { id: 1 },
+    update: {},
+    create: {
+      id: 1,
+      heightCm: 50,
+      widthCm: 40,
+      depthCm: 30,
+      weightG: 1000,
+    },
+  });
+
+  const concordia = await prisma.earthLocation.upsert({
+    where: { id: 1 },
+    update: {},
+    create: {
+      id: 1,
+      address1: "1455 boul de Maisonneuve O",
+      city: "Montreal",
+      administartiveArea: "QC",
+      postalCode: "H3G 1M8",
+      countryCode: "CA",
+      lat: 45.497276341920866,
+      lng: -73.57893824773116,
+    },
+  });
+
+  const loyola = await prisma.earthLocation.upsert({
+    where: { id: 2 },
+    update: {},
+    create: {
+      id: 2,
+      address1: "7141 rue Sherbrooke O",
+      city: "Montreal",
+      administartiveArea: "QC",
+      postalCode: "H4B 1R6",
+      countryCode: "CA",
+      lat: 45.45837647581191,
+      lng: -73.63861101850817,
+    },
+  });
+
+  const quote = await prisma.quotation.upsert({
+    where: { id: 1 },
+    update: {},
+    create: {
+      id: 1,
+      originId: concordia.id,
+      destinationId: loyola.id,
+      amountQuotedCents: 2000,
+      boxId: box.id,
+    },
+  });
+
+  const shipment = await prisma.shipmentTransaction.upsert({
+    where: { id: 1 },
+    update: {},
+    create: {
+      id: 1,
+      trackingNumber: "111111111111",
+      quotationId: quote.id,
+      transactionType: TransactionType.MANUAL,
+      transactionDetail: "Seeded",
+      shipperId: customer.id,
+    },
+  });
+
+  const pickup = await prisma.trackingEvent.upsert({
+    where: { id: 1 },
+    update: {},
+    create: {
+      id: 1,
+      type: TrackingStatus.PICKED_UP_AT_ORIGIN,
+      locationId: concordia.id,
+      shipmentTransactionId: shipment.id,
+    },
+  });
+}
 
 async function main() {
-  const {
-    values: { environment },
-  } = parseArgs({ options: { environment: { type: "string" } } });
+  // const {
+  //   values: { environment },
+  // } = parseArgs({ options: { environment: { type: "string" } } });
 
-  switch (environment) {
-    case "production":
-    case "develop":
-      await seedAll();
-      break;
-    case "preview":
-      await seedAll();
-      break;
-    default:
-      console.log(
-        `Invalid environment "${environment}". Please provide a valid environment.`,
-      );
-      break;
-  }
+  // switch (environment) {
+  //   case "production":
+  //   case "develop":
+  //   case "development":
+  //     await seedAll();
+  //     break;
+  //   case "preview":
+  //     await seedAll();
+  //     break;
+  //   default:
+  //     console.log(
+  //       `Invalid environment "${environment}". Please provide a valid environment.`,
+  //     );
+  //     break;
+  // }
+  seedAll();
 }
 
 main();
