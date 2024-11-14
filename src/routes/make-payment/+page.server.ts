e8cee5cad08f364eade692ae07060fcc95b7b951
Giuliano <giulianoverdone@gmail.com>
2024-11-14 14:43:06 -0500

    Integrate with Quotation feature

--- src/routes/make-payment/+page.server.ts
+++ src/routes/make-payment/+page.server.ts
@@ -1,1 +1,35 @@
-// loading quotation to client, and superforms logic + redirect to /api/stripe/checkout will all go here.
+import { QuotationRepository } from "$lib/domain/QuotationRepository";
+import type { PageServerLoad } from "./$types";
+
+export const load: PageServerLoad = async ({ url }) => {
+  // Extract the quotationId from the URL parameters
+  const quotationIdParam = url.searchParams.get("quotationId");
+  let quotationData = null;
+
+  if (quotationIdParam) {
+    const quotationId = parseInt(quotationIdParam, 10);
+
+    if (!isNaN(quotationId)) {
+      // Fetch the quotation data using the repository
+      const quotation = await QuotationRepository.findById(quotationId);
+
+      // Convert it to POJO
+      if (quotation) {
+        quotationData = {
+          id: quotation.id,
+          createdAt: quotation.createdAt,
+          updatedAt: quotation.updatedAt,
+          originId: quotation.originId,
+          destinationId: quotation.destinationId,
+          amountQuotedCents: quotation.amountQuotedCents,
+          etaDays: quotation.etaDays,
+          boxId: quotation.boxId,
+        };
+      }
+    }
+  }
+
+  return {
+    quotationData,
+  };
+};
