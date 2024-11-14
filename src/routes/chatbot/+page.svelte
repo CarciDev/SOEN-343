4ccad0a4468b41f51f7915106d09b9198640461d
Giuliano <giulianoverdone@gmail.com>
2024-11-14 18:40:58 -0500

    Update chatbot UI

--- src/routes/chatbot/+page.svelte
+++ src/routes/chatbot/+page.svelte
@@ -3,6 +3,12 @@
 </script>
 
 <main>
-  <h1>My SvelteKit App</h1>
-  <ChatBox />
+  <div class="mx-auto mb-10 max-w-2xl text-center">
+    <h1 class="mb-6 text-5xl sm:text-6xl">ChatBot - Customer Service</h1>
+    <p class="text-lg">
+      Need assistance? SvelteShipSolutions is proud to offer the latest and
+      greatest in AI-driven customer support, direclty at your fingertips.
+    </p>
+    <ChatBox />
+  </div>
 </main>
