0cbe328e43a170dc2eace01cb77a441a9d709479
Giuliano <giulianoverdone@gmail.com>
2024-11-23 11:55:18 -0500

    Update index.ts

--- src/lib/components/map/index.ts
+++ src/lib/components/map/index.ts
@@ -1,6 +1,7 @@
 import type L from "leaflet";
 import Map from "./map.svelte";
 import Marker from "./marker.svelte";
+import RoutingMachine from "./routingMachine.svelte";
 import { key } from "./key";
 
 export type MapContext = {
@@ -8,4 +9,4 @@ export type MapContext = {
   getMap: () => L.Map;
 };
 
-export { Map, Marker, key };
+export { Map, Marker, RoutingMachine, key };
